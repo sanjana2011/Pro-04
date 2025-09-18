b2df2740a98521efb4545fee332da9a309dec475
sanjana_2011 <sanjanagangrade20816@acropolis.in>
2025-09-18 16:04:08 +0530

    done

---
+++ ORSProject/src/main/java/in/co/rays/Proj4/controller/ErrorCtl.java
@@ -0,0 +1,33 @@
+
+package in.co.rays.Proj4.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import in.co.rays.Proj4.util.ServletUtility;
+
+/**
+ * @author Sanjana Gangrade
+ *
+ */
+public class ErrorCtl extends BaseCtl {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		ServletUtility.forward(getView(), request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		ServletUtility.forward(getView(), request, response);
+	}
+
+	@Override
+	protected String getView() {
+		return ORSView.ERROR_VIEW;
+	}
+}
